670ba125fb2d181f0c2a6035639419da9aa4487d
Jakob Lierman <jakob.lierman@gmail.com>
2025-06-24 15:53:57 +0200

    chore: upgrade eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line filenames-simple/naming-convention -- Will be renamed (ESLint v9)
 module.exports = {
   extends: ['@jakoblierman/eslint-config'],
+  rules: {
+    'unicorn/prefer-module': 'off',
+  },
 };
